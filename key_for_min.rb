6ff5a3a7d9c906e3e3e9a994b287cca099872d65
Victoria Meng <nights1ststar@protonmail.com>
2018-03-10 22:00:30 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,5 +5,6 @@ def key_for_min_value(name_hash)
   values = []
   name_hash.each { |key, value| values << value }
   min_value = values.inject { |memo, value| memo < value ? memo : value }
-  name_hash.rassoc(min_value).first
+  min_pair = name_hash.rassoc(min_value)
+  min_pair[0]
 end
